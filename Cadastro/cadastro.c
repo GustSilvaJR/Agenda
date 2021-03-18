23ccd9b1498fe6142c5ab60957b66264e3034c39
Gustavo Alessandro <gust.offic@gmail.com>
2021-03-18 14:09:19 -0300

    Defining the type of presentation function like void and deleting .exe file

--- Cadastro/cadastro.c
+++ Cadastro/cadastro.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-intro(){	
+void intro(){	
 	
 	printf(" ------------------------------------------------------------- \n");
 	printf("|                          AGENDA                             |\n");
